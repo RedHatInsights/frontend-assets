123c00655fadc0ff7d94b15527ea4a73a5356cb5
Karel Hala <khala@redhat.com>
2025-05-06 18:03:25 +0200

    Add security scan file

---
+++ security-scan.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+###########################
+# This script sources the security-scan.sh script from 
+# https://github.com/RedHatInsights/platform-security-gh-workflow
+# This script, in combination with Jenkins, scans a repo's Dockerfile
+# to provide a Software Bill of Materials (SBOM) and scan security vulnerabilities.
+###########################
+
+set -exv
+
+IMAGE_NAME="frontend-assets"
+DOCKERFILE_LOCATION="./build-tools"
+
+# (Severity Options: negligible, low, medium, high, critical)
+FAIL_ON_SEVERITY="high"
+
+# Build on "podman" or "docker"
+PODMAN_OR_DOCKER="docker"
+
+# Dockerfile Name
+DOCKERFILE_NAME="$PWD/build-tools/Dockerfile"
+
+ls -lah $PWD/build-tools
+
+curl -sSL https://raw.githubusercontent.com/RedHatInsights/platform-security-gh-workflow/master/jenkins/security-scan.sh | \
+    sh -s "${IMAGE_NAME}" "${DOCKERFILE_LOCATION}" "${FAIL_ON_SEVERITY}" "${PODMAN_OR_DOCKER}" "${DOCKERFILE_NAME}"
